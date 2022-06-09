9563af89eae874a5afc427d78ec2b5f53fa0edd5
Khazimla <20113497@rcconnect.co.za>
2022-06-09 18:09:24 +0200

    last changes

--- data/data.js
+++ data/data.js
@@ -3,12 +3,57 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var userTemplateElem = document.querySelector(".userTemplate").innerHTML;
     var compileTem = Handlebars.compile(userTemplateElem);
-    let getData = JSON.parse(localStorage.getItem("clientData"));
-    // get a reference to the template script tag
+  // get a reference to the template script tag
     var templateSource = document.querySelector(".userData");
-console.log(typeof getData);
     // compile the template
+    let getData = localStorage.getItem('clientData') ? JSON.parse(localStorage.getItem('clientData')) : [];
+    let depMap = localStorage.getItem('departments') ? JSON.parse(localStorage.getItem('departments')) : {};
+const objectKeys = Object.keys(depMap)
+const objectValues = Object.values(depMap)
+// alert(objectValues)
     var userTemplateHtml = compileTem({userData : getData})   
-    console.log(userTemplateHtml)
     templateSource.innerHTML = userTemplateHtml
+
+
+    const chartElem = document.getElementById('myChart')
+
+
+
+
+const myChartElem = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(myChartElem, {
+    type: 'bar',
+    data: {
+            labels: objectKeys,
+        datasets: [{
+            label: '# of Votes',
+            data: objectValues,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
 });
--- dom.js
+++ dom.js
@@ -1,31 +1,40 @@
 let name1 = document.querySelector(".enteredName")
 const service = document.querySelector(".serv")
 const date = document.querySelector(".dateForm")
-const button = document.querySelector(".btns")
+const buttonElem = document.querySelector(".btns")
 let data = document.querySelector('.first');
-
+// alert(buttonElem)
 var select = document.getElementById('services');
 let dataSet = localStorage.getItem('clientData') ? JSON.parse(localStorage.getItem('clientData')) : [];
+let depMap = localStorage.getItem('departments') ? JSON.parse(localStorage.getItem('departments')) : {};
 
+buttonElem.addEventListener("click", function () {
 
-button.addEventListener("click", function () {
-
-  // alert('hello');
   let firstName = name1.value
-  // alert(firstName)
+    // alert(date.value)
   var services = select.options[select.selectedIndex].value;
   let currentDate = new Date().toDateString();
-  // data.value  = name1.value +  value + currentDate.toDateString()
-  // document.getElementById('value').value = option.value;
+  let appoinmentDate = date.value;
+
+ if(depMap[services] == undefined){
+  depMap[services] = 0;
+ }
+ depMap[services]++
 
 
-  if (firstName, services, currentDate) {
+  if (firstName, services, appoinmentDate) {
 
     dataSet.push({
-      firstName, services, currentDate
+      firstName, services, currentDate, appoinmentDate
     })
   }
   localStorage.setItem("clientData", JSON.stringify(dataSet))
+  // alert(depMap.services)
+  localStorage.setItem("departments", JSON.stringify(depMap))
 
 })
+
+
+
+
 
